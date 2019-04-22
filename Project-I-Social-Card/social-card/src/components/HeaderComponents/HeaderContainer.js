6b88690c37f6aded7b89db202e1a5bd013adb3fc
harjothkhara <harjoth.khara@gmail.com>
2019-04-22 14:19:57 -0700

    initial commit

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderContainer = () => {
+return (
+    <div className="header-container">
+        <HeaderContent />
+        <HeaderTitle />
+        <ImageThumbnail />   
+    </div>
+  )  
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -3,4 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
+
+function App() {
+
+    return (
+        <div className="App">
+    
+  
+        </div> 
+ );
+}
+
+    
+    
+
 ReactDOM.render(<App />, document.getElementById('root'));
