4b7b7eda26989a219b586fd3e923bb5928391fd1
harjothkhara <harjoth.khara@gmail.com>
2019-04-23 14:18:49 -0700

    working components created with props

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.scss';
+
+const CalculatorDisplay = props => {
+    return (
+        <div>
+            {props.solution}
+        </div>
+    )
+
+}
+
+export default CalculatorDisplay;
